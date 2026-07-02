6911f3fcdf13bce60379e4722dc289d1d5f32ccc
yaoylu <alysa.yy.lu@gmail.com>
2026-07-02 15:03:52 +0800

    Add GitHub link to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ export default function Home() {
     <div>
       <h1>Home</h1>
       <p>Welcome to Next.js Learning!</p>
+      <p style={{ marginTop: 16 }}>
+        <a href="https://github.com/yaoylu/nextjs-learning" target="_blank" rel="noopener noreferrer">
+          GitHub Repository
+        </a>
+      </p>
     </div>
   )
 }
